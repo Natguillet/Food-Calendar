9ec56127f92b1b8f098517b690a3c6240b183329
ENSEMUQAC <pierremasquilier@gmail.com>
2017-11-19 15:49:43 -0500

    Commit : inscription par mail et identification

--- app/src/main/java/uqac/natacha/food_calendar/Accueil.java
+++ app/src/main/java/uqac/natacha/food_calendar/Accueil.java
@@ -1,17 +1,25 @@
 package uqac.natacha.food_calendar;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,6 +54,8 @@ public class Accueil extends AppCompatActivity {
 
     private boolean estEnHaut = false;
     private int angleRotation = 0;
+    private FirebaseAuth auth;
+    private EditText inputEmail, inputPassword, inputPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +70,13 @@ public class Accueil extends AppCompatActivity {
         //
         myView.setVisibility(View.INVISIBLE);
 
+        //on récupère l'utilisateur courant
+        auth = FirebaseAuth.getInstance();
+
     }
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -172,6 +188,56 @@ public class Accueil extends AppCompatActivity {
      * @param view le bouton
      */
     public void onInscriptionClick(View view) {
+
+
+         inputEmail = (EditText) findViewById(R.id.til_pseudo_inscription);
+        inputPassword = (EditText) findViewById(R.id.til_mdp_inscription);
+        inputPassword2 = (EditText) findViewById(R.id.til_mdp_inscription2);
+
+        if (TextUtils.isEmpty(inputEmail.getText().toString().trim())) {
+
+            Toast.makeText(Accueil.this, "Veuillez rentrer une adresse mail", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(inputPassword.getText().toString().trim())) {
+            Toast.makeText(Accueil.this, "Veuillez rentrer un mot de passe", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(inputPassword2.getText().toString().trim())) {
+            Toast.makeText(Accueil.this, "Veuillez confirmer votre mot de passe", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (inputPassword.getText().toString().trim().length() < 6) {
+            Toast.makeText(Accueil.this, "Veuillez rentrer un mot de passe d'au moins 6 caractères ! ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (inputPassword.getText().toString().trim().length() != inputPassword2.getText().toString().trim().length()) {
+            Toast.makeText(Accueil.this, "Les deux mots de passe ne sont pas identiques", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(inputEmail.getText().toString().trim(), inputPassword.getText().toString().trim())
+                .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Accueil.this, "L'enregistrement a échoué. Veuillez essayer avec une autre adresse mail.", Toast.LENGTH_SHORT).show();
+                            return;
+
+                        } else {
+
+                            Toast.makeText(Accueil.this, "Enregistrement a réussi ! Vous avez reçu un email de confirmation.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
         //TODO Pierre
     }
 
@@ -180,6 +246,44 @@ public class Accueil extends AppCompatActivity {
      * @param view
      */
     public void onConnexionClick(View view) {
-        //TODO Pierre
+
+        inputEmail = (EditText) findViewById(R.id.til_pseudo);
+        inputPassword = (EditText) findViewById(R.id.til_mdp);
+
+        if (TextUtils.isEmpty(inputEmail.getText().toString().trim())){
+            Toast.makeText(Accueil.this, "Enregistrement a réussi ! Vous avez reçu un email de confirmation.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(inputPassword.getText().toString().trim())) {
+            Toast.makeText(Accueil.this, "Enregistrement a réussi ! Vous avez reçu un email de confirmation.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        final Task<AuthResult> authResultTask = auth.signInWithEmailAndPassword(inputEmail.getText().toString().trim(), inputPassword.getText().toString().trim())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Accueil.this, "L'identification a échoué !", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            Toast.makeText(Accueil.this, "L'identification a réussi !", Toast.LENGTH_SHORT).show();
+
+
+                            // GO TO MAIN HERE
+
+
+                            ///
+
+
+
+
+                            return;
+                        }
+                    }
+                });
+
     }
 }
