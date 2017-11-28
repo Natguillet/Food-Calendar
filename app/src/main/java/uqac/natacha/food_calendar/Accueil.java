cd5f270409060df14225e11b3c4f4c42dd42d8e7
ENSEMUQAC <pierremasquilier@gmail.com>
2017-11-27 19:56:04 -0500

    Bug corrigé : Connexion

--- app/src/main/java/uqac/natacha/food_calendar/Accueil.java
+++ app/src/main/java/uqac/natacha/food_calendar/Accueil.java
@@ -24,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import uqac.natacha.food_calendar.Modele.ShoppingList;
 
 /**
  * Created by Florian on 24/10/2017.
@@ -233,7 +234,10 @@ public class Accueil extends AppCompatActivity {
 
                         } else {
 
-                            Toast.makeText(Accueil.this, "Enregistrement a réussi ! Vous avez reçu un email de confirmation.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Accueil.this, "Enregistrement a réussi !", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Accueil.this, Accueil.class);
+                            startActivity(intent);
+
 
                         }
                     }
@@ -248,8 +252,8 @@ public class Accueil extends AppCompatActivity {
      */
     public void onConnexionClick(View view) {
 
-        inputEmail = (EditText) findViewById(R.id.til_pseudo);
-        inputPassword = (EditText) findViewById(R.id.til_mdp);
+        inputEmail = (EditText) findViewById(R.id.eti_pseudo);
+        inputPassword = (EditText) findViewById(R.id.eti_mdp);
 
         if (TextUtils.isEmpty(inputEmail.getText().toString().trim())){
             Toast.makeText(Accueil.this, "Veuillez rentrer une adresse mail ! ", Toast.LENGTH_SHORT).show();
